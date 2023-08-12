be8f1f74d6b41b65024a6df52fe9d6489c934d6f
Jay Cabasag <jaycabasag1999@gmail.com>
2023-08-12 23:04:45 +0800

    style: add UI on Element landing page

--- src/pages/element/ElementPage.tsx
+++ src/pages/element/ElementPage.tsx
@@ -1,5 +1,6 @@
 import { Link, useParams } from "react-router-dom"
 import PageTitle from "../../components/compositions/home/PageTitle"
+import { Pencil2Icon, ReaderIcon } from "@radix-ui/react-icons"
 
 export default function ElementPage() {
   const { elementId  } = useParams()
@@ -7,33 +8,32 @@ export default function ElementPage() {
     <div className="w-full h-full flex flex-col items-center justify-center py-12">
        <PageTitle text={`NTC Reviewer - ${elementId}`}/>
        <p className="max-w-xl px-2 mx-auto text-center text-[14px] md:text-base text-gray-600">
-        Welcome to NTC Reviewer Unofficial! 
-        Your source for insights and information 
-        related to the National Telecommunication Commission
+        Radio Rules and Regulations
       </p>
-       <div className="mt-[20px] max-w-7xl w-full flex items-center justify-center flex-col">
-         <p className="my-[20px]">Please select an action : </p>
-        <div>
-       <Link 
-        to={'review'}
-       >
-         <button
-          type="button"
-          className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
-         >
-            Review
-          </button>
-       </Link>
-       <Link 
-        to={'exam'}
-       >
-         <button
-           type="button"
-           className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
-         >
-            Exam
-         </button>
-       </Link>
+       <div className="mt-[20px] px-4 max-w-7xl w-full flex items-center justify-center flex-col">
+        <div className="flex flex-col md:flex-row w-full max-w-sm px-[20px] gap-0 md:gap-4">
+          <Link 
+            to={'review'}
+          >
+            <button
+              type="button"
+              className="flex items-center justify-center gap-2 w-full md:min-w-[200px] text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+            >
+                <ReaderIcon />
+                Review
+              </button>
+          </Link>
+          <Link 
+            to={'exam'}
+          >
+            <button
+              type="button"
+              className="flex items-center justify-center gap-2 w-full md:min-w-[200px] text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+            >
+                <Pencil2Icon />
+                Take Exam
+            </button>
+          </Link>
         </div>
        </div>
     </div>
