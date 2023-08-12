8d73e5d5c2fa00c3a1d7f1385cee847abb88e8b2
Jay Cabasag <jaycabasag1999@gmail.com>
2023-08-12 23:12:47 +0800

    style: fix components

---
+++ src/components/compositions/element/ElementPageCallToAction.tsx
@@ -0,0 +1,40 @@
+import { Pencil2Icon, ReaderIcon } from '@radix-ui/react-icons'
+import { Link } from 'react-router-dom'
+
+const callToActions = [
+    {
+        label: 'review',
+        link: 'review',
+        icon: <ReaderIcon />
+    },
+    {
+        label: 'exam',
+        link: 'exam',
+        icon: <Pencil2Icon />
+    }
+] as const
+
+export default function ElementPageCallToAction() {
+  return (
+    <div className="mt-[20px] px-4 max-w-7xl w-full flex items-center justify-center flex-col">
+    <div className="flex flex-col md:flex-row w-full max-w-sm px-[20px] gap-0 md:gap-4">
+      {callToActions.map(action => {
+        return (
+            <Link
+                to={action.link}
+                key={action.link}
+            >
+                <button
+                  type="button"
+                  className="flex items-center justify-center gap-2 w-full md:min-w-[200px] text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+                >
+                    {action.icon}
+                    {action.label}
+                </button>
+            </Link>
+        )
+      })}
+    </div>
+   </div>
+  )
+}
--- src/pages/element/ElementPage.tsx
+++ src/pages/element/ElementPage.tsx
@@ -1,6 +1,6 @@
-import { Link, useParams } from "react-router-dom"
+import { useParams } from "react-router-dom"
 import PageTitle from "../../components/compositions/home/PageTitle"
-import { Pencil2Icon, ReaderIcon } from "@radix-ui/react-icons"
+import ElementPageCallToAction from "../../components/compositions/element/ElementPageCallToAction"
 
 export default function ElementPage() {
   const { elementId  } = useParams()
@@ -10,32 +10,7 @@ export default function ElementPage() {
        <p className="max-w-xl px-2 mx-auto text-center text-[14px] md:text-base text-gray-600">
         Radio Rules and Regulations
       </p>
-       <div className="mt-[20px] px-4 max-w-7xl w-full flex items-center justify-center flex-col">
-        <div className="flex flex-col md:flex-row w-full max-w-sm px-[20px] gap-0 md:gap-4">
-          <Link 
-            to={'review'}
-          >
-            <button
-              type="button"
-              className="flex items-center justify-center gap-2 w-full md:min-w-[200px] text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
-            >
-                <ReaderIcon />
-                Review
-              </button>
-          </Link>
-          <Link 
-            to={'exam'}
-          >
-            <button
-              type="button"
-              className="flex items-center justify-center gap-2 w-full md:min-w-[200px] text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
-            >
-                <Pencil2Icon />
-                Take Exam
-            </button>
-          </Link>
-        </div>
-       </div>
+      <ElementPageCallToAction />
     </div>
   )
 }
